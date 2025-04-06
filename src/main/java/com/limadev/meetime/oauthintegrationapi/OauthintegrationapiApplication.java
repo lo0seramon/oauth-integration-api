0de4bbba7663df21f1ea91575b18781b17c7422c
Ramon Lima <lima.ramon13@gmail.com>
2025-04-05 21:55:10 -0300

    Initial commit

---
+++ src/main/java/com/limadev/meetime/oauthintegrationapi/OauthintegrationapiApplication.java
@@ -0,0 +1,13 @@
+package com.limadev.meetime.oauthintegrationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthintegrationapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthintegrationapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/limadev/meetime/oauthintegrationapi/OauthintegrationapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limadev.meetime.oauthintegrationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthintegrationapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

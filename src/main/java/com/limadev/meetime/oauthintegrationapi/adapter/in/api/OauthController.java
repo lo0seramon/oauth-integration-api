bb03236d4fc5ed2c0f369f7dca94e855bcd4400a
Ramon Lima <lima.ramon13@gmail.com>
2025-04-09 14:59:51 -0300

    Implementing swagger documentations

--- src/main/java/com/limadev/meetime/oauthintegrationapi/adapter/in/api/OauthController.java
+++ src/main/java/com/limadev/meetime/oauthintegrationapi/adapter/in/api/OauthController.java
@@ -1,9 +1,13 @@
 package com.limadev.meetime.oauthintegrationapi.adapter.in.api;
 
+import com.limadev.meetime.oauthintegrationapi.adapter.in.api.response.AuthorizeResponse;
 import com.limadev.meetime.oauthintegrationapi.application.business.port.in.GetAuthorizationUrlUseCase;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/oauth")
 @AllArgsConstructor
+@Tag(name = "Oauth Controller", description = "Controller para o fluxo de autenticação")
 public class OauthController {
 
     private GetAuthorizationUrlUseCase getAuthorizationUrlUseCase;
 
+    @ApiResponse(
+            description = "Retorno de sucesso para a criação de url",
+            responseCode = "200",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = AuthorizeResponse.class))
+    )
     @GetMapping("v1/authorize")
-    public ResponseEntity<String> getAuthorizationUrl() {
-        String authorizationUrl = getAuthorizationUrlUseCase.getAuthorizationUrl();
-        return new ResponseEntity<>(authorizationUrl, HttpStatus.OK);
+    public ResponseEntity<AuthorizeResponse> getAuthorizationUrl() {
+        return ResponseEntity.ok().body(AuthorizeResponse
+                .builder()
+                .authorizationUrl(getAuthorizationUrlUseCase.getAuthorizationUrl())
+                .build());
     }
 }
